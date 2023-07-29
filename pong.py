d4834e0baee481471163deba8b27cc0574e2e8d0
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-07-29 17:00:21 +0500

    player movement

--- pong.py
+++ pong.py
@@ -2,10 +2,13 @@ import pygame
 
 # pygame setup
 pygame.init()
+fps = 60
 width, height = 1280, 720
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pong")
 running = True
+clock = pygame.time.Clock()
+dt = 0 # delta time in seconds since last frame
 
 # colors
 TEAL = (0, 128, 128)
@@ -18,9 +21,9 @@ player_width = 10
 player_height = 100
 player_speed = 2
 
-player1_x = (width - player_width) - 50
+player1_x = player_width + 50
 player1_y = height // 2
-player2_x = player_width + 50
+player2_x = (width - player_width) - 50
 player2_y = height // 2
 
 # game loop
@@ -36,7 +39,20 @@ while running:
     pygame.draw.rect(screen, TEAL, (player1_x, player1_y, player_width, player_height))
     pygame.draw.rect(screen, TEAL, (player2_x, player2_y, player_width, player_height))
 
+    # move player
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player1_y -= 300 * dt * player_speed
+    if keys[pygame.K_s]:
+        player1_y += 300 * dt * player_speed
+
+    if keys[pygame.K_UP]:
+        player2_y -= 300 * dt * player_speed
+    if keys[pygame.K_DOWN]:
+        player2_y += 300 * dt * player_speed
+
     # flip() the display to put your work on screen
     pygame.display.flip()
+    dt = clock.tick(fps) / 1000
 
 pygame.quit()
